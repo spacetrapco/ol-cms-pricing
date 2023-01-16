5afdf9559cb04fc4a47d293914679c86f5252dfd
Spacetrap Studio <119510419+spacetrapco@users.noreply.github.com>
2023-01-16 20:19:18 +0700

    geotargetly-selector

---
+++ geotargetly-selector.js
@@ -0,0 +1,40 @@
+// check if geotargetly class for United States is visible
+if (document.getElementsByClassName("geotargetlygeocontent1659511329942_content_1")[0].style.display === "inline") {
+  // check if attribute 1-yearly has no value
+  if (!document.getElementsByClassName("starter-radio-custom")[0].getAttribute("1-yearly")) {
+    // add hide class to the highest parent
+    document.getElementsByClassName("radio-button-field-4")[0].classList.add("hide");
+  }
+}
+// check if geotargetly class for Australia is visible
+if (document.getElementsByClassName("geotargetlygeocontent1659511329942_content_2")[0].style.display === "inline") {
+  // check if attribute 2-yearly has no value
+  if (!document.getElementsByClassName("starter-radio-custom")[0].getAttribute("2-yearly")) {
+    // add hide class to the highest parent
+    document.getElementsByClassName("radio-button-field-4")[0].classList.add("hide");
+  }
+}
+// check if geotargetly class for Singapore is visible
+if (document.getElementsByClassName("geotargetlygeocontent1659511329942_content_3")[0].style.display === "inline") {
+  // check if attribute 3-yearly has no value
+  if (!document.getElementsByClassName("starter-radio-custom")[0].getAttribute("3-yearly")) {
+    // add hide class to the highest parent
+    document.getElementsByClassName("radio-button-field-4")[0].classList.add("hide");
+  }
+}
+// check if geotargetly class for Malaysia is visible
+if (document.getElementsByClassName("geotargetlygeocontent1659511329942_content_4")[0].style.display === "inline") {
+  // check if attribute 4-yearly has no value
+  if (!document.getElementsByClassName("starter-radio-custom")[0].getAttribute("4-yearly")) {
+    // add hide class to the highest parent
+    document.getElementsByClassName("radio-button-field-4")[0].classList.add("hide");
+  }
+}
+// check if geotargetly class for Indonesia is visible
+if (document.getElementsByClassName("geotargetlygeocontent1659511329942_content_5")[0].style.display === "inline") {
+  // check if attribute 5-yearly has no value
+  if (!document.getElementsByClassName("starter-radio-custom")[0].getAttribute("5-yearly")) {
+    // add hide class to the highest parent
+    document.getElementsByClassName("radio-button-field-4")[0].classList.add("hide");
+  }
+}
