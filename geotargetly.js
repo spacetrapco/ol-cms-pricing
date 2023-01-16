210d6335521e925b5f342135bad6efa137f1094e
Spacetrap Studio <119510419+spacetrapco@users.noreply.github.com>
2023-01-16 20:42:01 +0700

    geotargetly

---
+++ geotargetly.js
@@ -0,0 +1,52 @@
+window.onload = function(){
+    const geotargetly = document.getElementsByClassName("geotargetlygeocontent1659511329942_content_1")[0];
+    if (geotargetly && geotargetly.style.display === "inline") {
+        const input = document.getElementsByClassName("starter-radio-custom")[0];
+        if (!input.getAttribute("1-yearly")) {
+            const parent = document.getElementsByClassName("radio-button-field-4")[0];
+            if(!parent.classList.contains("hide")){
+                parent.classList.add("hide");
+            }
+        }
+    }
+    const geotargetly2 = document.getElementsByClassName("geotargetlygeocontent1659511329942_content_2")[0];
+    if (geotargetly2 && geotargetly2.style.display === "inline") {
+        const input2 = document.getElementsByClassName("starter-radio-custom")[0];
+        if (!input2.getAttribute("2-yearly")) {
+            const parent2 = document.getElementsByClassName("radio-button-field-4")[0];
+            if(!parent2.classList.contains("hide")){
+                parent2.classList.add("hide");
+            }
+        }
+    }
+    const geotargetly3 = document.getElementsByClassName("geotargetlygeocontent1659511329942_content_3")[0];
+    if (geotargetly3 && geotargetly3.style.display === "inline") {
+        const input3 = document.getElementsByClassName("starter-radio-custom")[0];
+        if (!input3.getAttribute("3-yearly")) {
+            const parent3 = document.getElementsByClassName("radio-button-field-4")[0];
+            if(!parent3.classList.contains("hide")){
+                parent3.classList.add("hide");
+            }
+        }
+    }
+    const geotargetly4 = document.getElementsByClassName("geotargetlygeocontent1659511329942_content_4")[0];
+    if (geotargetly4 && geotargetly4.style.display === "inline") {
+        const input4 = document.getElementsByClassName("starter-radio-custom")[0];
+        if (!input4.getAttribute("4-yearly")) {
+            const parent4 = document.getElementsByClassName("radio-button-field-4")[0];
+            if(!parent4.classList.contains("hide")){
+                parent4.classList.add("hide");
+            }
+        }
+    }
+    const geotargetly5 = document.getElementsByClassName("geotargetlygeocontent1659511329942_content_4")[0];
+    if (geotargetly5 && geotargetly5.style.display === "inline") {
+        const input5 = document.getElementsByClassName("starter-radio-custom")[0];
+        if (!input5.getAttribute("5-yearly")) {
+            const parent5 = document.getElementsByClassName("radio-button-field-4")[0];
+            if(!parent5.classList.contains("hide")){
+                parent5.classList.add("hide");
+            }
+        }
+    }
+}
